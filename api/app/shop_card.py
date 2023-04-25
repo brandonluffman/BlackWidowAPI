118147f8f8ea52baa1cf6baa80d9a00edadce7ac
Brandon Luffman <brandonluff10@gmail.com>
2023-04-25 12:15:00 -0400

    shopping card fi

--- api/app/shop_card.py
+++ api/app/shop_card.py
@@ -1,8 +1,8 @@
 from requests_html import HTMLSession
 import requests
 
-# entities = ['uniqlo airism ultra']
-entities = ['nike dri-fit reluxe boxer briefs', 'everlane boxer brief.', 'exofficio', 'hanes comfortflex waistband boxer brief', 'uniqlo airism ultra', 'j.crew stretch boxer briefs.', 'tommy john second skin relaxed fit boxer', 'calvin klein microfiber stretch boxer briefs', 'ralph lauren classic-fit boxer', 'saxx relaxed-fit boxer briefs']
+entities = ['saxx relaxed-fit boxer briefs']
+# entities = ['nike dri-fit reluxe boxer briefs', 'everlane boxer brief.', 'exofficio', 'hanes comfortflex waistband boxer brief', 'uniqlo airism ultra', 'j.crew stretch boxer briefs.', 'tommy john second skin relaxed fit boxer', 'calvin klein microfiber stretch boxer briefs', 'ralph lauren classic-fit boxer', 'saxx relaxed-fit boxer briefs']
 domain = 'https://www.google.com/search?tbm=shop&hl=en&q='
 entity_links = [(domain + entity.replace(' ', '+'),entity) for entity in entities]
 final_card_links = []
@@ -27,7 +27,7 @@ for item in entity_links:
         link_count = 0
         ### For Loop Below loops through queries to find Shopping Link and Integer Representing Amounnt of Stores that are linked to that product ###
         for product_result in product_results[:5]:
-            print(product_result)
+            # print(prod'uct_result)
             product_link = 'https://www.google.com' + product_result.find(css_identifier_link, first=True).attrs['href']
             product_compare = product_result.find(css_identifier_test_2, first=True)
             product_review_count = product_result.find(css_product_reviews, first=True).text
@@ -54,11 +54,38 @@ for item in entity_links:
                         }
                         output.append(cards)
                         link_count += 1
+                    else:
+                        continue
             else:
-                continue
+                if product_review_count:
+                    print(product_review_count)
+                    if len(product_review_count.split()) > 3:
+                        review_num = int(product_review_count.split()[5].replace(',',''))
+                    else:
+                        review_num = False
+                    if review_num:
+                        cards = {
+                        'Data' : product_link, 
+                        'Count' : 0,
+                        'Review Count' : review_num,
+                        'entity': entity
+                        }
+                        output.append(cards)
+                    else:
+                        continue
+                else:
+                    print('no prooduct revieew count')
+
 
         if not output:
-            final_card_links.append(product_results[0])
+            product_link = 'https://www.google.com' + product_result.find(css_identifier_link, first=True).attrs['href']
+            card = {
+                        'Data' : product_link, 
+                        'Count' : 0,
+                        'Review Count' : 0,
+                        'entity': entity,
+                    }
+            final_card_links.append(card)
         else:
             counts = []
             for out in output:
@@ -76,8 +103,6 @@ for item in entity_links:
             if index_len == 1:
                 max_index = max_indexes[0]
             print('Index Len -----> ', index_len)
-            
-            print(index_len)
 
             max_review_count = []
             if index_len > 1:
